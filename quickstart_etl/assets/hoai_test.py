de7537796abdb5408a672119476d17cba4a9036f
Hoai Pham <19732942+hoaipham97@users.noreply.github.com>
2025-07-30 14:03:24 +0700

    Create hoai_test.py

---
+++ quickstart_etl/assets/hoai_test.py
@@ -0,0 +1,20 @@
+import base64
+import json
+import os
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+from dagster import AssetExecutionContext, MaterializeResult, MetadataValue, asset
+
+
+@asset(group_name="hoaipham_test", compute_kind="hoaipham_test Dagster")
+def test1() -> None:
+  print("ahihi")
+
+
+@asset(deps=[test1], group_name="hoaipham_test", compute_kind="hoaipham_test Dagster")
+def test2(context: AssetExecutionContext) -> MaterializeResult:
+  print("test2_hihi")
+  return 1 + 3
